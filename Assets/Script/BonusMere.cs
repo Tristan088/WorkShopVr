9e06c7686911851e9d461e8263c6f125cdc832cb
Wazercube <wazercube_34@outlook.fr>
2019-05-31 12:35:57 +0200

    Création classe mère Bonus

---
+++ Assets/Script/BonusMere.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusMere : MonoBehaviour
+{
+    public int ammos;
+    public float timerBonus;
+    protected float timerLeft;
+
+
+    public void ActiveBonus()
+    {
+
+    }
+
+    public void ShootBonus()
+    {
+        if(--ammos<=0)
+        {
+            GetRaketBack();
+        }
+        
+    }
+
+    public void GetRaketBack()
+    {
+        
+    }
+}
